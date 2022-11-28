0daa0b24a5f2b7efba2b591dcc9bbeac34376836
Sebastien <sebastien.jensen@proton.me>
2022-11-28 22:26:00 +0000

    Cmd parser

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-var cli = document.getElementById('gameBox');
-var output = new Typewriter(cli, {loop: false, delay: getDelay()});
+var currentRoom = "wspace"
+var textBox = document.getElementById('gameBox');
+var output = new Typewriter(textBox, {loop: false, delay: getDelay()});
 
 function getDelay(){
   return (Math.floor(Math.random() * (50 - 40 + 1) + 40));
+}
+
+function checkInput(e){
+  if(e.key == 'Enter'){
+    command = userInput.textContent; // use the typed command
+    userInput.innerHTML = ""
+    parser(command);
+    e.preventDefault();
+  }
+}
+
+function parser(cmd){
+  let commandWords = cmd.trim().toUpperCase().split(" ");
+  switch (commandWords.length) {
+    case 1:
+      switch (commandWords[0]) {
+        case "TEST":
+          output.typeString("Test lol").start()
+      }
+  }
 }
 
 output.typeString("<strong style='font-size:24pt'>AGONY IN AMSTERDAM</strong>")
   .pauseFor(200)
   .typeString("<br>version ID-0.1 by @sjedev")
   .pauseFor(2500)
-  .deleteAll()
+  .deleteAll(1)
   .typeString("The detective thanks you as you hang up the phone and flick on the TV for the local news. “A death in the square”, the headlines read, but the reports lacked information. You have a job to do, so you quickly grab your jacket and cycle to the police station.").start();
